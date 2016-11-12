79645fcaf8436732e825c10cd2dc2605ddef6f82
“your_username <your_registered_github_Email>
2016-11-12 20:43:31 +0800

    1112

---
+++ 242.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        int a[26]={0};
+        for(int i=0;i<s.size();i++)
+        {
+        	a[s[i]-'a']++;
+		}
+		for(int i=0;i<t.size();i++)
+		{
+			a[t[i]-'a']--;
+		}
+		for(int i=0;i<26;i++)
+		{
+			if(a[i]!=0)
+			{
+				return false;
+			}
+		}
+		return true;
+    }
+};
+int main()
+{
+	Solution test;
+	cout << test.isAnagram("anagram","nagaram");
+} 
